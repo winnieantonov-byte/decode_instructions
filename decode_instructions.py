851cdadb72a600fe0f2bcaf4210b5570ada54125
winnieantonov-byte <winnieantonov@gmail.com>
2026-02-08 01:28:34 +0300

    Update decode_instructions.py

--- decode_instructions.py
+++ decode_instructions.py
@@ -1,24 +1,25 @@
-#156529281
-def decode_instructions(encrypted_str: str, i=0) -> tuple[str, int]:
-    result = ""
-    multiplier = 0  
-    while i < len(encrypted_str):
-        char = encrypted_str[i]
-        if char.isdigit():
-            multiplier = multiplier * 10 + int(char)
-        elif char == '[':
-            substring, next_i = decode_instructions(encrypted_str, i + 1)
-            result += substring * (multiplier if multiplier > 0 else 1)
-            multiplier = 0
-            i = next_i
-        elif char == ']':
-            return result, i
-        else:
-            result += char
-        i += 1
-    return result, i
+#156551479
+def decode_instruction(data):
+    def solve(pos=0):
+        result = ''
+        multiplier = ''
+        while pos < len(data):
+            char = data[pos]
+            if char.isdigit():
+                multiplier += char
+            elif char == '[':
+                fragment, pos = solve(pos + 1)
+                result += fragment * int(multiplier if multiplier else 1)
+                multiplier = ''
+            elif char == ']':
+                return result, pos
+            else:
+                result += char
+            pos += 1
+        return result, pos
+
+    return solve()[0]
 
 if __name__ == '__main__':
-    commands = input()
-    decoded_str, _ = decode_instructions(commands)
-print(decoded_str)
+    print(decode_instruction(input()))
+
